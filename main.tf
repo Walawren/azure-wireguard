7bbfaf1d55762d5c06bb5055d955875e6d95a006
Eric Carlson <e.carlson94@gmail.com>
2020-07-07 04:16:07 -0600

    fix formatting

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ provider azurerm {
   features {}
 }
 
- terraform {
-   backend "remote" {
-     # The name of your Terraform Cloud organization.
-     organization = "Walawren"
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Walawren"
 
-     # The name of the Terraform Cloud workspace to store Terraform state files in.
-     workspaces {
-       name = "azure-wireguard"
-     }
-   }
- }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "azure-wireguard"
+    }
+  }
+}
 
 resource azurerm_resource_group rgrp {
   name     = "AzureWireGuard"
