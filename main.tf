1fcdb10dd2422e6ba04246dbef9b927830bbb31d
Eric Carlson <e.carlson94@gmail.com>
2020-07-07 02:36:06 -0600

    create resource group, add action definition

---
+++ main.tf
@@ -0,0 +1,16 @@
+ terraform {
+   backend "remote" {
+     # The name of your Terraform Cloud organization.
+     organization = "Walawren"
+
+     # The name of the Terraform Cloud workspace to store Terraform state files in.
+     workspaces {
+       name = "azure-wireguard"
+     }
+   }
+ }
+
+resource azurerm_resource_group rgrp {
+  name     = "AzureWireGuard"
+  location = "West Central US"
+}
