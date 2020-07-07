af8d7d3aeb09c8053a85c2ca9e990d0d7cc98cf9
Eric Carlson <e.carlson94@gmail.com>
2020-07-07 13:54:23 -0600

    nsg rule fix

--- nsg.tf
+++ nsg.tf
@@ -26,30 +26,6 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "VirtualNetwork"
     destination_port_range     = 22
   }
-
-  security_rule {
-    name                       = "AllowInternetOutbound"
-    priority                   = 100
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_address_prefix = "Internet"
-    destination_port_range     = 443
-  }
-
-  security_rule {
-    name                       = "DenyAllOutbound"
-    priority                   = 4000
-    direction                  = "Outbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_address_prefix      = "*"
-    source_port_range          = "*"
-    destination_address_prefix = "*"
-    destination_port_range     = "*"
-  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnet_association" {
