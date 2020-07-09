a235fc7cc051ae60e9a2717a71a379d9bcf73fa1
Eric Carlson <e.carlson94@gmail.com>
2020-07-09 14:46:22 -0600

    fix naming mismatch

--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@ locals {
   lower_rgrp_name = lower(azurerm_resource_group.rgrp.name)
 }
 
-resource "azurerm_storage_account" "storage" {
+resource "azurerm_storage_account" "sa" {
   name                     = "${local.lower_rgrp_name}sa"
   resource_group_name      = azurerm_resource_group.rgrp.name
   location                 = azurerm_resource_group.rgrp.location
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "wireguard_confs" {
   name                  = "${local.lower_rgrp_name}confs"
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
 
