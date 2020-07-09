36848fc6a9d9f98d48afb9cd9fcc9483d01656e8
Eric Carlson <e.carlson94@gmail.com>
2020-07-09 14:42:21 -0600

    add storage account, and logic app managed identity

--- rbac.tf
+++ rbac.tf
@@ -1,10 +1,11 @@
+## VM Managed User
 resource "azurerm_user_assigned_identity" "vm" {
   name                = "${azurerm_resource_group.rgrp.name}-VM-Identity"
   resource_group_name = azurerm_resource_group.rgrp.name
   location            = azurerm_resource_group.rgrp.location
 }
 
-resource "azurerm_role_assignment" "vm_user" {
+resource "azurerm_role_assignment" "vm_admin" {
   scope = azurerm_resource_group.rgrp.id
 
   role_definition_name = "Virtual Machine Administrator Login"
@@ -24,5 +25,19 @@ resource "azurerm_key_vault_access_policy" "vm_identity" {
     "delete",
     "list"
   ]
+}
+
+## Logic App Managed User
+resource "azurerm_user_assigned_identity" "logic_app" {
+  name                = "${azurerm_resource_group.rgrp.name}-LogicApp-Identity"
+  resource_group_name = azurerm_resource_group.rgrp.name
+  location            = azurerm_resource_group.rgrp.location
+}
+
+resource "azurerm_role_assignment" "storage_reader" {
+  scope = azurerm_storage_container.wireguard_confs.id
+
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_user_assigned_identity.logic_app.principal_id
 }
 
---
+++ storage.tf
@@ -0,0 +1,18 @@
+locals {
+  lower_rgrp_name = lower(azurerm_resource_group.rgrp.name)
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${local.lower_rgrp_name}sa"
+  resource_group_name      = azurerm_resource_group.rgrp.name
+  location                 = azurerm_resource_group.rgrp.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "wireguard_confs" {
+  name                  = "${local.lower_rgrp_name}confs"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
